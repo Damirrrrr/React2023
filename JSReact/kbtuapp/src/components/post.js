409b0f50a0e6862c41077a7e741fefd1a42449b0
Damirrrrr <amanzholovdamir@inbox.ru>
2023-10-28 15:55:01 +0600

    Midkaaa

--- JSReact/kbtuapp/src/components/post.js
+++ JSReact/kbtuapp/src/components/post.js
@@ -4,8 +4,20 @@ export const Postpage = () => {
 
     return(
         <div className='posts'>
-            <div className='posts__innner'>
-                feio
+            <div className='container'>
+                <div className='posts__inner'>
+                    <div className='post__create'>
+                        <textarea className='post__text' placeholder="What's Happening?" />
+                        <button role='button'> Tweet</button>
+                    </div>
+                    <div className='post__list'>
+                        <div className='post__item'>
+                            <div className='post__content'>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
